9c111b185314719c2b6f725102f88cadd2e87b6b
ruslanvs <ruslanvs@yahoo.com>
2018-05-22 13:28:12 -0700

    Initial commit, blank project, no storiboard

---
+++ contacts_fetching/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  contacts_fetching
+//
+//  Created by Ruslan Suvorov on 5/22/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
+
